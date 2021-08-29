efdc740ce4548adc0fe23d97832609d58695485d
Cheshan782 <cheshan.cu@gmail.com>
2021-08-29 08:50:23 -0400

    First Version

---
+++ Other/index.js
@@ -0,0 +1,7 @@
+const handleFirstTab = (e) => {
+    if(e.key==='Tab'){
+        document.body.classList.add('user-is-tabbing')
+        window.removeEventListener('keydown', handleFirstTab)
+        window.addEventListener('mousedown', handleMouseDownOnce)
+    }
+}
